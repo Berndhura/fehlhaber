668afef612a5140d64585a12bd7790bdbada4c62
Bernd Wichura <wichura@gmx.de>
2020-06-06 22:11:44 +0200

    firestore first test

--- app/src/main/java/com/example/fehlhaber/MainActivity.java
+++ app/src/main/java/com/example/fehlhaber/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.fehlhaber;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.pdf.PdfDocument;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -21,6 +23,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
+    private PdfDocument pdfDoc = new PdfDocument();
+    //https://www.youtube.com/watch?v=RjpFwkfRM3U
+
+    private Button okButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,33 +36,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button sendData = findViewById(R.id.saveData);
         sendData.setOnClickListener(this);
 
+        okButton = findViewById(R.id.okButton);
+        okButton.setVisibility(View.GONE);
+
     }
 
     @Override
     public void onClick(View v) {
 
-        EditText lastNAmeView = findViewById(R.id.lastName);
-        String lastName = lastNAmeView.getText().toString();
+        EditText lastNameView = findViewById(R.id.lastName);
+        String lastName = lastNameView.getText().toString();
 
-        // Create a new user with a first and last name
+        EditText nameView = findViewById(R.id.firstName);
+        String name = nameView.getText().toString();
+        Log.d("maul", "Name: " + name);
+
+        EditText plzView = findViewById(R.id.plz);
+        String plz = plzView.getText().toString();
+
+        // Create a new user
         Map<String, Object> user = new HashMap<>();
-        user.put("first", "Ada");
+        user.put("first", name);
         user.put("last", lastName);
-        user.put("born", 1815);
+        user.put("plz", plz);
 
         // Add a new document with a generated ID
-        db.collection("users")
-                .add(user)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+        db.collection("users").document(lastName)
+                .set(user)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Log.d("maul", "DocumentSnapshot added with ID: " + documentReference.getId());
+                    public void onSuccess(Void aVoid) {
+                        Log.d("maul", "DocumentSnapshot successfully written!");
+                        okButton.setVisibility(View.VISIBLE);
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        Log.w("maul", "Error adding document", e);
+                        Log.w("maul", "Error writing document", e);
                     }
                 });
     }
